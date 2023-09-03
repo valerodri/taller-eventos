1548c1391c7cf2317dc9996ef156473d87943f2b
Valerodri <134001586+Valerodri@users.noreply.github.com>
2023-09-03 18:05:38 -0300

    Solucion click

--- eventos.js
+++ eventos.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const div = document.getElementById("div");
  
-    div.addEventListener("click", function() {
+    div.addEventListener("click", function(event) {
+       if (!event.target.matches("button")) {
            alert("Hola! Soy el div");
        }
-   )});
- 
+   });
+ });
  
